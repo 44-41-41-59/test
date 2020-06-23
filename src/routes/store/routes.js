c9df8bb864df7df43118a3b7b22b8126cfde3d8b
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-06-24 01:00:50 +0300

    permissions comments

--- src/routes/store/routes.js
+++ src/routes/store/routes.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 const { addStore, editStore, deleteStore, getAllStores, getOneStore, getOwnerAllStores, getPendingStores} = require('./store.js');
 
-// create new store by USER/ get all stores by USER
+// create new store by USER/ get all stores by USER // bearer and permission to create store // public
 router.route('/store').post(addStore).get(getAllStores);
-// get all pending stores in the admin dashboard
+// get all pending stores in the admin dashboard // bearer and permission to get admin dashboard
 router.route('/store/admin/dashboard').get(getPendingStores);
-// get all the stores owned by one owner by USER
+// get all the stores owned by one owner by USER // public
 router.route('/store/owner/:owner_id').get(getOwnerAllStores);
-// get one store by store ID by USER/  edit one store by OWNER / edit one store status by ADMIN / delete one store by OWNER and ADMIN 
+// get one store by store ID by USER public /  edit one store by OWNER permission update store / edit one store status by ADMIN edit store status permission/ delete one store by OWNER and ADMIN permission delete store
 router.route('/store/:id').get(getOneStore).put(editStore).patch(editStore).delete(deleteStore);
 
 module.exports = router;
--- src/routes/whishlist/routes.js
+++ src/routes/whishlist/routes.js
@@ -4,11 +4,11 @@ const express = require('express');
 const router = express.Router();
 const { getUserWishlist, addProductsToWishlist, deleteFromWishlist} = require('./wishlist');
 
-//post an item to the wishlist of the user
+//post an item to the wishlist of the user // bearer // permission create wishlist item
 router.route('/wishlist').post(addProductsToWishlist);
-// update or delete one wishlist item
+// update or delete one wishlist item // bearer // permission delete wishlist item
 router.route('/wishlist/:id').delete(deleteFromWishlist);
-// get all items of wishlist for one user
+// get all items of wishlist for one user // public
 router.route('/wishlist/user/:userID').get(getUserWishlist);
 
 module.exports = router;

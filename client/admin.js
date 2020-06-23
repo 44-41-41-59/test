50a22d5ba8b65859ba4dae847cbe87ee0b688c9c
YazanAlaiwah <yazanalaiwah@gmail.com>
2020-06-24 02:04:24 +0300

    add the chat

---
+++ client/admin.js
@@ -0,0 +1,101 @@
+const uuid = require('uuid').v4;
+const io = require('socket.io-client');
+const inquirer = require('inquirer');
+
+let room = uuid();
+const socket = io.connect('http://localhost:3000');
+socket.userid = uuid();
+socket.on('connect', async () => {
+  let masseges = [];
+  let name = await getName();
+
+  socket.on('next', async (room) => {
+    console.clear();
+    masseges = [];
+    console.log('the user was end the chat do u want have the next client y/n');
+    const response = await inquirer.prompt([
+      {
+        prefix: '',
+        name: 'text',
+        message: `--bobo---\n`,
+      },
+    ]);
+
+    const command = response.text.toLowerCase().split(' ')[0];
+    switch (command) {
+      case 'y':
+        socket.emit('next', room);
+        break;
+      case 'n':
+        socket.emit('admindisconecct', room);
+        process.exit();
+        break;
+    }
+  });
+
+  socket.on('masseage', (massege) => {
+    console.clear();
+    masseges.push(massege);
+    masseges.forEach((massege) => {
+      massege.id === socket.id
+        ? console.log(' ' + massege.message)
+        : console.log(
+            '\x1b[33m%s\x1b[0m',
+            '                                                   ' +
+              massege.message
+          );
+    });
+    console.log('');
+  });
+  socket.emit('admin', name, room);
+
+  socket.on('news', (data) => {
+    console.log(data);
+    socket.emit('my other event', 'yazan', 'ahmed', (data) => {
+      console.log(data);
+    });
+  });
+  socket.emit('say', 'hello there');
+
+  getInput();
+
+  async function getInput() {
+    const response = await inquirer.prompt([
+      {
+        prefix: '',
+        name: 'text',
+        message: `----------------\n`,
+      },
+    ]);
+    const command = response.text.toLowerCase().split(' ')[0];
+    switch (command) {
+      case 'quit':
+        socket.emit('userDisconnected', { room });
+        process.exit();
+        break;
+      // case 'y':
+      //   socket.emit('next', room);
+      //   break;
+      // case 'n':
+      //   socket.emit('admindisconecct', room);
+      //   process.exit();
+      //   break;
+      default:
+        socket.emit('massege', room, response.text, socket.id);
+        getInput();
+        break;
+    }
+    // socket.emit('message', `${response.text}`);
+    // getInput();
+  }
+});
+
+/// السروات
+async function getName() {
+  console.clear();
+  const input = await inquirer.prompt([
+    { name: 'name', message: 'What is your name?' },
+  ]);
+  return input.name;
+  // getInput();
+}
--- src/server.js
+++ src/server.js
@@ -19,6 +19,10 @@ const paymentHistory = require('./routes/payment/payment-history/routes.js');
 
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server); 
+
+
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.static('./public'));
@@ -55,6 +59,68 @@ app.use(errorHandeler);
 module.exports = {
   server: app,
   start: (port) => {
-    app.listen(port, () => console.log(`Hearing from port -> ${port}`));
+    server.listen(port);
+    // app.listen(port, () => console.log(`Hearing from port -> ${port}`));
   },
 };
+
+
+let clientsQueue = [];
+let custmerRoom = {};
+io.on('connection', (socket) => {
+  socket.on('massege', (room, message, id) => {
+    console.log(room);
+    io.in(room).emit('masseage', { message, id });
+  });
+
+  socket.on('error', (payload) => {
+    console.log('error', payload);
+  });
+  socket.on('admin', (name, room) => {
+    custmerRoom[room] = { status: true, name };
+    socket.join(room);
+    console.log(custmerRoom);
+    setTimeout(() => {
+      loop();
+    }, 1000);
+  });
+  socket.on('next', (room) => {
+    if (custmerRoom[room]) custmerRoom[room].status = true;
+    setTimeout(() => {
+      loop();
+    }, 1000);
+  });
+  socket.on('admindisconecct', (room) => {
+    delete custmerRoom[room];
+    console.log(custmerRoom);
+  });
+  socket.on('userConnected', async (name) => {
+    clientsQueue.push(socket);
+
+    if (!loop()) socket.emit('wait', {});
+  });
+  socket.on('userDisconnected', (room) => {
+    socket.leave(room.room);
+
+    io.in(room.room).emit('next', room.room);
+
+    console.log(custmerRoom);
+  });
+});
+
+function loop() {
+  if (clientsQueue.length) {
+    for (let room in custmerRoom) {
+      if (custmerRoom[room].status) {
+        let name = custmerRoom[room].name;
+        let socket = clientsQueue.shift();
+        socket.join(room);
+        socket.emit('joinded', { room, name });
+        custmerRoom[room].status = false;
+        console.log('sreved', custmerRoom);
+        return room;
+      }
+    }
+    return false;
+  }
+}

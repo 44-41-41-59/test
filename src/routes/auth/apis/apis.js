d4d911a4d0c2c4102e7cc39360c6ffa7e445abf8
YazanAlaiwah <yazanalaiwah@gmail.com>
2020-06-30 20:34:10 +0300

    make the nodemodiler

--- src/routes/auth/apis/apis.js
+++ src/routes/auth/apis/apis.js
@@ -3,26 +3,81 @@
 const { userCollection } = require('../../../DB/users/user-model.js');
 const fetch = require('node-fetch');
 const userSchema = require('../../../DB/users/user-schema.js');
+const nodemailer = require('nodemailer');
+const xoauth2 = require('xoauth2');
 
 // sign up function
 async function signup(req, res, next) {
   let record;
-  try {
-    let check = await userCollection.read(req.body);
-    if (check.status === 401) {
-      record = await userCollection.create(req.body);
-      req.acl = {
-        acl: record.acl.capabilities,
-      };
+  // try {
+  //   let check = await userCollection.read(req.body);
+  //   if (check.status === 401) {
+  //     record = await userCollection.create(req.body);
+  //     req.acl = {
+  //       acl: record.acl.capabilities,
+  //     };
 
-      res.json({ data: record, acl: req.acl });
-    } else {
-      throw Error('user already signed up');
+  //     res.json({ data: record, acl: req.acl });
+  //   } else {
+  //     throw Error('user already signed up');
+  //   }
+  const output = `
+    <h1>HELLO FROM DAAY-mall team its work yeah😍😍😊🥰</h1>
+  `;
+
+  // const output = `
+  //     <p>You have a new contact request</p>
+  //     <h3>Contact Details</h3>
+  //     <ul>
+  //       <li>Name: ${req.body.name}</li>
+  //       <li>Company: ${req.body.company}</li>
+  //       <li>Email: ${req.body.email}</li>
+  //       <li>Phone: ${req.body.phone}</li>
+  //     </ul>
+  //     <h3>Message</h3>
+  //     <p>${req.body.message}</p>
+  //   `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    // host: 'mail.google.com',
+    // port: 587,
+    // secure: false, // true for 465, false for other ports
+    service: 'gmail',
+    auth: {
+      // xoauth2: xoauth2.createXOAuth2Generator({
+      user: 'eng.yazanalaiwah@gmail.com', // generated ethereal user
+      pass: 'yazan55235570', // generated ethereal password
+      // }),
+    },
+    // tls: {
+    //   rejectUnauthorized: false,
+    // },
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"DAAY-mall" <eng.yazanalaiwah@gmail.com>', // sender address
+    to: 'eng.yazanalaiwah@gmail.com', // list of receivers
+    subject: 'DAAY-mall test', // Subject line
+    text: 'Hello world?', // plain text body
+    html: output, // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
     }
-  } catch (e) {
-    console.log({ status: 500, message: e.message });
-    next({ status: 500, message: e.message });
-  }
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    res.send('contact');
+  });
+  // } catch (e) {
+  //   console.log({ status: 500, message: e.message });
+  //   next({ status: 500, message: e.message });
+  // }
 }
 
 // sign in function

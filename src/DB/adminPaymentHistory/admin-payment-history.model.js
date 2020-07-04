8b6c951a59c10de7bfdb77798251846f3003a6a6
YazanAlaiwah <yazanalaiwah@gmail.com>
2020-07-05 00:21:47 +0300

    make delte for the order schema in some time

--- src/DB/adminPaymentHistory/admin-payment-history.model.js
+++ src/DB/adminPaymentHistory/admin-payment-history.model.js
@@ -1,12 +1,16 @@
+/* eslint-disable comma-dangle */
 'use strict';
+const moment = require('moment');
 const schema = require('./admin-payment-history.schema.js');
+const { CronJob } = require('cron');
+
 class AdminPaymentHistory {
   constructor() {
     this.schema = schema;
   }
   read(queryObject = {}) {
     return this.schema.findOne(queryObject).populate('orders');
-  }  
+  }
   async create(obj) {
     let record = new this.schema(obj);
     let result = record.save();
@@ -14,6 +18,34 @@ class AdminPaymentHistory {
   }
   async update(_id, record) {
     return this.schema.findOneAndUpdate(_id, record, { new: true });
+  }
+  async removeLoop(step, type) {
+    let obj = {
+      minutes: `*/${step} * * * *`,
+      hours: `* */${step} * * *`,
+      days: `* * */${step} * *`,
+    };
+    console.log(obj[type]);
+    var job = new CronJob(
+      obj[type],
+      function () {
+        var older_than = moment().subtract(step, type).toDate();
+        // // var older_than2 = moment().subtract(9, 'minutes').toDate();
+        this.schema.updateMany(
+          { Timestamp: { $lte: older_than } },
+          { orders: [] },
+          { new: true }
+        );
+        this.schema.remove({
+          Timestamp: { $lte: older_than },
+          invalid: { $exists: true, $size: 0 },
+        });
+      },
+      null,
+      true,
+      'America/Los_Angeles'
+    );
+    job.start();
   }
 }
 module.exports = new AdminPaymentHistory();
--- src/routes/seedRoles/routes/routes.js
+++ src/routes/seedRoles/routes/routes.js
@@ -1,54 +1,57 @@
+/* eslint-disable comma-dangle */
 'use strict';
 const express = require('express');
 const router = express.Router();
 const Roles = require('../../../DB/schemas/roles');
 const payments = require('../../../DB/adminPaymentHistory/admin-payment-history.model.js');
-const {order} = require('../../../DB/collection-models.js');
+const { order } = require('../../../DB/collection-models.js');
 
+router.route('/orderloop').delete(removeLoop);
 
+router.route('/complaint').patch(addComplaint);
 
+router.route('/post/roles').post(addRoles);
 
-async function addComplaint(req, res, next){
-  try{
+async function addComplaint(req, res, next) {
+  try {
     let orderID = [];
     let _id;
-    let adminOrder = await payments.read({ paymentsHistory:  req.body.paymentHistoryID});
+    let adminOrder = await payments.read({
+      paymentsHistory: req.body.paymentHistoryID,
+    });
     console.log(adminOrder);
-    for(let i = 0; i < adminOrder.orders.length;i++){
+    for (let i = 0; i < adminOrder.orders.length; i++) {
       let index = adminOrder.orders[i].products.includes(req.body.productID);
-      if(index) {
+      if (index) {
         _id = adminOrder._id;
         orderID = adminOrder.orders[i]._id;
         adminOrder.invalid.push(adminOrder.orders[i]._id);
-        adminOrder.orders.splice(i,1);
+        adminOrder.orders.splice(i, 1);
       }
     }
 
-    let update = await payments.update({_id},adminOrder);
-    let record =  await order.update(orderID,{status:'bolcked'});
-    res.send(`we block the orders with ids ${orderID} and informe the store with id ${record._id} `);
-  }catch(e){
+    let update = await payments.update({ _id }, adminOrder);
+    let record = await order.update(orderID, { status: 'bolcked' });
+    res.send(
+      `we block the orders with ids ${orderID} and informe the store with id ${record._id} `
+    );
+  } catch (e) {
     console.log(e.message);
-    next({status:500,message:e.message});
+    next({ status: 500, message: e.message });
   }
-
 }
 
-router.route('/complaint').patch(addComplaint);
-router.route('/post/roles').post(async (req, res, next) => {
+function removeLoop(req, res) {
+  let { step, type } = req.body;
+  payments.removeLoop(step, type);
+  res.send(
+    `the data will be removed from admin-payment-history every ${step} ${type}`
+  );
+}
+
+async function addRoles(req, res, next) {
   try {
-    let roles = {
-      user: ['addToWishlist', 'deleteFromWishlist', 'addReview', 'updateReview',
-        'deleteReview', 'checkoutCart', 'readPaymentHistory', 
-        'deletePaymentHistory', 'addToFavorite', 'deleteFromFavorite',
-        'addCart', 'updateCart', 'deleteCart', 'readUserCart',
-      ],
-      owner: [
-        'createStore', 'updateStore', 'deleteStore', 'createProduct', 'updateProduct', 'deleteProduct', 'readOrders', 'updateOrder', 'deleteOrder',
-      ],
-      admin: ['deleteStore', 'updateStoreStatus', 'readPendingStores', 'readOrders',
-      ],
-    };
+    let roles = rolesFunction();
     for (let key in roles) {
       let record = new Roles({ role: key, capabilities: roles[key] });
       record = await record.save();
@@ -57,6 +60,44 @@ router.route('/post/roles').post(async (req, res, next) => {
   } catch (e) {
     next({ status: 500, message: e.message });
   }
-});
+}
+
+function rolesFunction() {
+  return {
+    user: [
+      'addToWishlist',
+      'deleteFromWishlist',
+      'addReview',
+      'updateReview',
+      'deleteReview',
+      'checkoutCart',
+      'readPaymentHistory',
+      'deletePaymentHistory',
+      'addToFavorite',
+      'deleteFromFavorite',
+      'addCart',
+      'updateCart',
+      'deleteCart',
+      'readUserCart',
+    ],
+    owner: [
+      'createStore',
+      'updateStore',
+      'deleteStore',
+      'createProduct',
+      'updateProduct',
+      'deleteProduct',
+      'readOrders',
+      'updateOrder',
+      'deleteOrder',
+    ],
+    admin: [
+      'deleteStore',
+      'updateStoreStatus',
+      'readPendingStores',
+      'readOrders',
+    ],
+  };
+}
 
 module.exports = router;

0342de35568d8924967f9470fdb575529f89e95f
YazanAlaiwah <yazanalaiwah@gmail.com>
2020-07-28 10:59:15 +0300

    update wishlist

--- src/DB/schemas/wishlist-schema.js
+++ src/DB/schemas/wishlist-schema.js
@@ -2,9 +2,12 @@
 const { Schema, model } = require('mongoose');
 
 const WishList = Schema({
-  userID:{type:Schema.Types.ObjectId},
-  productID:{type:Schema.Types.ObjectId, ref : 'product'},
+  userID: { type: Schema.Types.ObjectId },
+  productID: { type: Schema.Types.ObjectId, ref: 'product' },
 });
-
+WishList.pre('find', function (next) {
+  this.populate('productID');
+  next();
+});
 
 module.exports = model('wishlist', WishList);

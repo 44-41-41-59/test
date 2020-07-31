3d6a5cecbc67746de0348c480f740b932e32d82a
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-07-31 17:02:09 +0300

    fixed populating review userID

--- src/DB/schemas/reviews-schema.js
+++ src/DB/schemas/reviews-schema.js
@@ -2,11 +2,20 @@
 const { Schema, model } = require('mongoose');
 
 const review = Schema({
-  userID: { type: String, required: true },
+  userID: { type: Schema.Types.ObjectId, required: true ,ref: 'user' },
   review: { type: String, required: true },
   rate: { type: Number, required: true, min: 0, max: 5 },
-  storeID: { type: String },
-  productID: { type: Schema.Types.ObjectId },
+  storeID: { type: Schema.Types.ObjectId, ref: 'store'},
+  productID: { type: Schema.Types.ObjectId, ref: 'product'},
+},
+{ toObject: { virtuals: true } },
+{ toJSON: { virtuals: true } },
+);
+
+review.pre('find', function () {
+  this.populate('userID');
+  // .populate('storeID')
+  // .populate('productID');
 });
 
 module.exports = model('review', review);

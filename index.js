c99b48b4aaea2f058941af0468cacaff0efd8b6b
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-08-10 12:02:45 +0300

    testing seeding

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/newdaaymall';
-  // MONGODB_URI=mongodb://heroku_3m7wrf9f:qmlc7bdtjbskh4f08pljt1legs@ds217799.mlab.com:17799/heroku_3m7wrf9f
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
